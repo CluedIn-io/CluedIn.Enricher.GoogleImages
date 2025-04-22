7762a86e744c0ae0a2a2a6c964e5ff02a3c29b42
Yi-Wei <86753236+ywei390@users.noreply.github.com>
2025-04-22 09:25:03 +0800

    feat: Add helptext and guidance notes (#13)

--- src/ExternalSearch.Providers.GoogleImages/Constants.cs
+++ src/ExternalSearch.Providers.GoogleImages/Constants.cs
@@ -11,6 +11,40 @@ namespace CluedIn.ExternalSearch.Providers.GoogleImages
         public const string ProviderName = "Google Images";
         public static readonly Guid ProviderId = Guid.Parse("fd6f802e-fe4f-42aa-9623-ba6369128e84");
 
+        public static readonly string Instruction = $$"""
+                                      [
+                                        {
+                                          "type": "bulleted-list",
+                                          "children": [
+                                            {
+                                              "type": "list-item",
+                                              "children": [
+                                                {
+                                                  "text": "Add the {{EntityTypeLabel.ToLower()}} to specify the golden records you want to enrich. Only golden records belonging to that {{EntityTypeLabel.ToLower()}} will be enriched."
+                                                }
+                                              ]
+                                            },
+                                            {
+                                              "type": "list-item",
+                                              "children": [
+                                                {
+                                                  "text": "Add the vocabulary keys to provide the input for the enricher to search for additional information. For example, if you provide the website vocabulary key for the Web enricher, it will use specific websites to look for information about companies. In some cases, vocabulary keys are not required. If you don't add them, the enricher will use default vocabulary keys."
+                                                }
+                                              ]
+                                            },
+                                            {
+                                              "type": "list-item",
+                                              "children": [
+                                                {
+                                                  "text": "Add the API key to enable the enricher to retrieve information from a specific API. For example, the BvD enricher requires an access key to authenticate with the BvD API."
+                                                }
+                                              ]
+                                            }
+                                          ]
+                                        }
+                                      ]
+                                      """;
+
         public struct KeyName
         {
             public const string ApiToken = "apiToken";
@@ -35,28 +69,32 @@ namespace CluedIn.ExternalSearch.Providers.GoogleImages
                     DisplayName = "API Key",
                     Type = "password",
                     IsRequired = true,
-                    Name = KeyName.ApiToken
+                    Name = KeyName.ApiToken,
+                    Help = "The key to authenticate access to the Google API.",
                 },
                 new()
                 {
                     DisplayName = $"Accepted {EntityTypeLabel}",
                     Type = "entityTypeSelector",
                     IsRequired = true,
-                    Name = KeyName.AcceptedEntityType
+                    Name = KeyName.AcceptedEntityType,
+                    Help = $"The {EntityTypeLabel.ToLower()} that defines the golden records you want to enrich (e.g., /Organization)."
                 },
                 new()
                 {
                     DisplayName = "Image Search Text",
                     Type = "vocabularyKeySelector",
                     IsRequired = false,
-                    Name = KeyName.ImageSearch
+                    Name = KeyName.ImageSearch,
+                    Help = "The vocabulary key that contains the image search text you want to enrich (e.g., organization.name)."
                 }
             }
         };
 
         public static IEnumerable<Control> Properties { get; set; } = new List<Control>();
 
-        public static Guide Guide { get; set; } = null;
+        public static Guide Guide { get; set; } = new() { Instructions = Instruction };
+
         public static IntegrationType IntegrationType { get; set; } = IntegrationType.Enrichment;
     }
 }
